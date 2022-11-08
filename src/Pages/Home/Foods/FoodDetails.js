127585ae8ba6cce61b9ebdf09def66f3ad6c1a0b
saklainMostake <saklain15-12003@diu.edu.bd>
2022-11-08 12:49:37 +0600

    push

--- src/Pages/Home/Foods/FoodDetails.js
+++ src/Pages/Home/Foods/FoodDetails.js
@@ -1,20 +1,36 @@
 import React from 'react';
+import { FaStar } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 
-const FoodDetails = ({food}) => {
+const FoodDetails = ({ food }) => {
     console.log(food);
-    const {title, img, description } = food
+    const { title, img, description, price, rating } = food
     return (
-    
-           <div>
-           <div className="card shadow-xl">
-            <figure><img src={img} alt="Shoes" /></figure>
-            <div className="card-body">
-                <h2 className=" text-2xl card-title">{title}</h2>
-                <p className='  font-semibold text-left'><h2 className='font-bold text-xl'>Description:</h2> {description.slice(0,100)+ '...' }</p>
+
+        <div>
+            <div className="card shadow-xl">
+                <figure><img src={img} alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className=" text-2xl card-title">{title}</h2>
+                    <p className='  font-semibold text-left'><h2 className='font-bold text-xl'>Description:</h2> {description.slice(0, 100) + '...'}</p>
+                    <div>
+                        <div className='flex justify-between'>
+                            <p>Price: {price}</p>
+                            <div className='flex'>
+                                <FaStar></FaStar><p className='ml-2'>{rating}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='mt-10 card-actions justify-center'>
+                <Link to={`/checkout/`}>
+                <button className=" btn btn-info ">View Details</button>
+                    </Link>
+                </div>
+               
+                </div>
             </div>
         </div>
-           </div>
     );
 };
 
--- src/Pages/Services/ServicesCard.js
+++ src/Pages/Services/ServicesCard.js
@@ -1,15 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaStar } from 'react-icons/fa';
 
 const ServicesCard = ({food}) => {
-    const {title, img, description } = food
+    const {title, img, description, price, rating } = food
     return (
         <div>
              <div className="card shadow-xl m-5">
             <figure><img src={img} alt="Shoes" /></figure>
             <div className="card-body">
                 <h2 className=" text-2xl card-title">{title}</h2>
-                <p className='  font-semibold text-left'><h2 className='font-bold text-xl'>Description:</h2> {description}</p>
+                <p className='  font-semibold text-left'><span className='font-bold text-xl'>Description:</span> {description.slice(0,100)+ '...' }</p>
+                <div className='flex justify-between'>
+                            <p>Price: {price}</p>
+                            <div className='flex'>
+                                <FaStar></FaStar>
+                                <p className='ml-2'>{rating}</p>
+                            </div>
+                        </div>
+                <div className='mt-10 card-actions justify-center'>
+                <Link to={`/checkout/`}>
+                <button className=" btn btn-info ">View Details</button>
+                    </Link>
+                </div>
+               
             </div>
+           
         </div>
         </div>
     );
